574f5bbeb9b649a87365a1ba03e28911bae3c393
bryan <bryan.guner@gmail.com>
2020-11-02 01:15:24 -0500

    Formatting

--- projects/d3/past-soln/data-structures-graph-intro-starter-master/problems/graph.js
+++ projects/d3/past-soln/data-structures-graph-intro-starter-master/problems/graph.js
@@ -1,4 +1,4 @@
-class Graph {
+ddclass Graph {
     constructor() {
         this.adjacency;
     }
--- projects/overflow/graph overflow/Examples/GraphVertex.js
+++ projects/overflow/graph overflow/Examples/GraphVertex.js
@@ -1,138 +1,143 @@
-import LinkedList from '../linked-list/LinkedList';
+import LinkedList from "../linked-list/LinkedList";
 
 export default class GraphVertex {
-  /**
-   * @param {*} value
-   */
-  constructor(value) {
-    if (value === undefined) {
-      throw new Error('Graph vertex must have a value');
+    /**
+     * @param {*} value
+     */
+    constructor(value) {
+        if (value === undefined) {
+            throw new Error("Graph vertex must have a value");
+        }
+
+        /**
+         * @param {GraphEdge} edgeA
+         * @param {GraphEdge} edgeB
+         */
+        const edgeComparator = (edgeA, edgeB) => {
+            if (edgeA.getKey() === edgeB.getKey()) {
+                return 0;
+            }
+
+            return edgeA.getKey() < edgeB.getKey() ? -1 : 1;
+        };
+
+        // Normally you would store string value like vertex name.
+        // But generally it may be any object as well
+        this.value = value;
+        this.edges = new LinkedList(edgeComparator);
     }
 
     /**
-     * @param {GraphEdge} edgeA
-     * @param {GraphEdge} edgeB
+     * @param {GraphEdge} edge
+     * @returns {GraphVertex}
      */
-    const edgeComparator = (edgeA, edgeB) => {
-      if (edgeA.getKey() === edgeB.getKey()) {
-        return 0;
-      }
+    addEdge(edge) {
+        this.edges.append(edge);
 
-      return edgeA.getKey() < edgeB.getKey() ? -1 : 1;
-    };
+        return this;
+    }
 
-    // Normally you would store string value like vertex name.
-    // But generally it may be any object as well
-    this.value = value;
-    this.edges = new LinkedList(edgeComparator);
-  }
+    /**
+     * @param {GraphEdge} edge
+     */
+    deleteEdge(edge) {
+        this.edges.delete(edge);
+    }
 
-  /**
-   * @param {GraphEdge} edge
-   * @returns {GraphVertex}
-   */
-  addEdge(edge) {
-    this.edges.append(edge);
+    /**
+     * @returns {GraphVertex[]}
+     */
+    getNeighbors() {
+        const edges = this.edges.toArray();
 
-    return this;
-  }
+        /** @param {LinkedListNode} node */
+        const neighborsConverter = (node) => {
+            return node.value.startVertex === this
+                ? node.value.endVertex
+                : node.value.startVertex;
+        };
 
-  /**
-   * @param {GraphEdge} edge
-   */
-  deleteEdge(edge) {
-    this.edges.delete(edge);
-  }
+        // Return either start or end vertex.
+        // For undirected graphs it is possible that current vertex will be the end one.
+        return edges.map(neighborsConverter);
+    }
 
-  /**
-   * @returns {GraphVertex[]}
-   */
-  getNeighbors() {
-    const edges = this.edges.toArray();
+    /**
+     * @return {GraphEdge[]}
+     */
+    getEdges() {
+        return this.edges
+            .toArray()
+            .map((linkedListNode) => linkedListNode.value);
+    }
 
-    /** @param {LinkedListNode} node */
-    const neighborsConverter = (node) => {
-      return node.value.startVertex === this ? node.value.endVertex : node.value.startVertex;
-    };
+    /**
+     * @return {number}
+     */
+    getDegree() {
+        return this.edges.toArray().length;
+    }
 
-    // Return either start or end vertex.
-    // For undirected graphs it is possible that current vertex will be the end one.
-    return edges.map(neighborsConverter);
-  }
+    /**
+     * @param {GraphEdge} requiredEdge
+     * @returns {boolean}
+     */
+    hasEdge(requiredEdge) {
+        const edgeNode = this.edges.find({
+            callback: (edge) => edge === requiredEdge,
+        });
 
-  /**
-   * @return {GraphEdge[]}
-   */
-  getEdges() {
-    return this.edges.toArray().map((linkedListNode) => linkedListNode.value);
-  }
+        return !!edgeNode;
+    }
 
-  /**
-   * @return {number}
-   */
-  getDegree() {
-    return this.edges.toArray().length;
-  }
+    /**
+     * @param {GraphVertex} vertex
+     * @returns {boolean}
+     */
+    hasNeighbor(vertex) {
+        const vertexNode = this.edges.find({
+            callback: (edge) =>
+                edge.startVertex === vertex || edge.endVertex === vertex,
+        });
 
-  /**
-   * @param {GraphEdge} requiredEdge
-   * @returns {boolean}
-   */
-  hasEdge(requiredEdge) {
-    const edgeNode = this.edges.find({
-      callback: (edge) => edge === requiredEdge,
-    });
+        return !!vertexNode;
+    }
 
-    return !!edgeNode;
-  }
+    /**
+     * @param {GraphVertex} vertex
+     * @returns {(GraphEdge|null)}
+     */
+    findEdge(vertex) {
+        const edgeFinder = (edge) => {
+            return edge.startVertex === vertex || edge.endVertex === vertex;
+        };
 
-  /**
-   * @param {GraphVertex} vertex
-   * @returns {boolean}
-   */
-  hasNeighbor(vertex) {
-    const vertexNode = this.edges.find({
-      callback: (edge) => edge.startVertex === vertex || edge.endVertex === vertex,
-    });
+        const edge = this.edges.find({ callback: edgeFinder });
 
-    return !!vertexNode;
-  }
+        return edge ? edge.value : null;
+    }
 
-  /**
-   * @param {GraphVertex} vertex
-   * @returns {(GraphEdge|null)}
-   */
-  findEdge(vertex) {
-    const edgeFinder = (edge) => {
-      return edge.startVertex === vertex || edge.endVertex === vertex;
-    };
+    /**
+     * @returns {string}
+     */
+    getKey() {
+        return this.value;
+    }
 
-    const edge = this.edges.find({ callback: edgeFinder });
+    /**
+     * @return {GraphVertex}
+     */
+    deleteAllEdges() {
+        this.getEdges().forEach((edge) => this.deleteEdge(edge));
 
-    return edge ? edge.value : null;
-  }
+        return this;
+    }
 
-  /**
-   * @returns {string}
-   */
-  getKey() {
-    return this.value;
-  }
-
-  /**
-   * @return {GraphVertex}
-   */
-  deleteAllEdges() {
-    this.getEdges().forEach((edge) => this.deleteEdge(edge));
-
-    return this;
-  }
-
-  /**
-   * @param {function} [callback]
-   * @returns {string}
-   */
-  toString(callback) {
-    return callback ? callback(this.value) : `${this.value}`;
-  }
+    /**
+     * @param {function} [callback]
+     * @returns {string}
+     */
+    toString(callback) {
+        return callback ? callback(this.value) : `${this.value}`;
+    }
 }
